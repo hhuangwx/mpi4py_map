aca91451038124f6b04c13766c6df3143e2e4b14
marius <mmillea@ucdavis.edu>
2016-11-03 15:13:16 +0100

    ran 2to3

--- mpi4py_map/tests/test_mpi4py_map.py
+++ mpi4py_map/tests/test_mpi4py_map.py
@@ -6,15 +6,15 @@ def power(x, y=2):
 
 
 def test_power():
-    result_parallel = mpi4py_map.map(power, range(50), y=2, debug=True)
+    result_parallel = mpi4py_map.map(power, list(range(50)), y=2, debug=True)
     result_serial = [power(i, y=2) for i in range(50)]
     assert result_serial == result_parallel, "Parallel result does not match direct one."
 
 
 
 if __name__ == "__main__":
-    print """Run this file with mpirun, e.g.:
-    mpirun -n 4 test_mpi4py_map"""
+    print("""Run this file with mpirun, e.g.:
+    mpirun -n 4 test_mpi4py_map""")
 
     test_power()
 
--- mpi4py_map/tests/test_mpi4py_map_except.py
+++ mpi4py_map/tests/test_mpi4py_map_except.py
@@ -6,18 +6,18 @@ def power(x, y=2):
 
 def power_except_on_3(x, y=2):
     if x == 3:
-        raise NotImplementedError, "Error on 3."
+        raise NotImplementedError("Error on 3.")
     return x**y
 
 def test_power_except():
-    result_parallel = mpi4py_map.map(power_except_on_3, range(50), y=2, debug=True)
+    result_parallel = mpi4py_map.map(power_except_on_3, list(range(50)), y=2, debug=True)
     result_serial = [power(i, y=2) for i in range(50)]
     result_serial[3] = None
     assert result_serial == result_parallel, "Parallel result does not match direct one."
 
 if __name__ == "__main__":
-    print """Run this file with mpirun, e.g.:
-    mpirun -n 4 test_mpi4py_map"""
+    print("""Run this file with mpirun, e.g.:
+    mpirun -n 4 test_mpi4py_map""")
 
     test_power_except()
 
